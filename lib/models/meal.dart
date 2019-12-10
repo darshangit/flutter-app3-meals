af2f59a1fdbef4b118cd20fd960f21d3dfeb8aeb
Darshan Satya <darshans1989@gmail.com>
2019-12-09 20:50:18 -0500

    MEals item page, image shown

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -4,32 +4,32 @@ enum Complexity { Simple, Challenging, Hard }
 
 enum Affordability { Affordable, Pricey, Luxurious }
 
-class Meal {
+ class Meal {
   final String id;
   final List<String> categories;
   final String title;
   final String imageUrl;
-  final List<String> ingrediants;
+  final List<String> ingredients;
   final List<String> steps;
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
-  final bool isGluetenFree;
+  final bool isGlutenFree;
   final bool isLactoseFree;
   final bool isVegan;
   final bool isVegetarian;
 
-  Meal(
+  const Meal(
       {@required this.id,
       @required this.categories,
       @required this.title,
       @required this.imageUrl,
-      @required this.ingrediants,
+      @required this.ingredients,
       @required this.steps,
       @required this.duration,
       @required this.complexity,
       @required this.affordability,
-      @required this.isGluetenFree,
+      @required this.isGlutenFree,
       @required this.isLactoseFree,
       @required this.isVegan,
       @required this.isVegetarian});
