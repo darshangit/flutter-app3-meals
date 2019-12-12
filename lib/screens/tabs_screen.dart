b549ae143f25da6356dc1f2551371d95cd817e30
Darshan Satya <darshans1989@gmail.com>
2019-12-11 21:29:39 -0500

    Names changes

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,7 +8,10 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
-  final List<Widget> _pages = [CategoriesScreen(), FavoriteScreen()];
+  final List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoriteScreen(), 'title': 'Your Favorite'}
+  ];
 
   int _selectedPageIndex = 0;
 
@@ -22,9 +25,9 @@ class _TabScreenState extends State<TabScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meals'),
+        title: Text(_pages[_selectedPageIndex]['title']),
       ),
-      body: _pages[_selectedPageIndex],
+      body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
