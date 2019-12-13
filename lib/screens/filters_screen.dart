69c25bfe1e65b37768f6d81ee100ca3768cca2c7
Darshan Satya <darshans1989@gmail.com>
2019-12-12 21:02:09 -0500

    Pushnamed

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app3_meals/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   
@@ -10,7 +11,7 @@ class FiltersScreen extends StatelessWidget {
       appBar: AppBar(
           title: Text('Filters'),
         ),
-      
+      drawer: MainDrawer(),
       body: Center(
       child: Text('Hello'),
     ));
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -35,9 +35,9 @@ class MainDrawer extends StatelessWidget {
             height: 20,
           ),
           buildListTile('Meals', Icons.restaurant, () {
-            Navigator.of(context).pushNamed('/');
+            Navigator.of(context).pushReplacementNamed('/');
           }),
-          buildListTile('Filters', Icons.settings, () {Navigator.of(context).pushNamed(FiltersScreen.routeName);})
+          buildListTile('Filters', Icons.settings, () {Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);})
         ],
       ),
     );
